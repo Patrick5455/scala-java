4199b9f47bf88229667fb621ee8e7b7c7ecb98d5
patrick ojunde <patrickojunde@gmail.com>
2021-03-07 01:46:53 +0100

    updates

--- scala-pluralsight/scala_getting_started/pratice_codes/src/Implicits.scala
+++ scala-pluralsight/scala_getting_started/pratice_codes/src/Implicits.scala
@@ -1,7 +1,24 @@
-object Implicits extends App { //extends App trait so we can run the main function in this class
 
-def stringToCurrency(){}
+case class Currency(code: String, amount: Double, toUSD:Double)
+
+object Implicits extends App {
 
 
+ implicit def stringToCurrency(money: String): Currency ={
+    //money "USD 556"
+    val Array(code:String, value: String) = money.split("\\s")
+    val amountAsDouble = value.toDouble
 
+    code match {
+      case "USD" => Currency("USD", amountAsDouble, amountAsDouble)
+      case "NZD" => Currency("USD", amountAsDouble, amountAsDouble *(1/1.5))
+      case "CAD" => Currency("USD", amountAsDouble, amountAsDouble *(1/1.30))
+    }
+}
+  println(stringToCurrency("USD 100"))
+  println(stringToCurrency("NZD 250"))
+
+  val cad : Currency = "CAD 100" //implicitly mapped to type currency
+  //using the implicit method  stringToCurrency
+  println(cad)
 }
