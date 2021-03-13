e92b5925ee181f6e2c7dedcb6c72ed6c9e7dbd0a
patrick ojunde <patrickojunde@gmail.com>
2021-03-13 10:54:09 +0100

    chapter 1

--- scala-for-java-developers/chapter1/src/main/scala/starters/Main.scala
+++
@@ -1,6 +0,0 @@
-package starters
-
-object Main extends App {
-  val ages = Seq(42, 75, 29, 64)
-  println(s"The oldest person is ${ages.max}") 
-}
---
+++ scala-for-java-developers/chp-0/src/main/scala/starters/Main.scala
@@ -0,0 +1,12 @@
+package starters
+
+object Main extends App {
+
+main(args: Array[String]){
+  val ages = Seq(42, 75, 29, 64)
+  println(s"The oldest person is ${ages.max}")
+
+}
+
+
+}
